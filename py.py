43ea34ded148169c0d8c230695f65ee612694e04
Carlos Araújo <111444349+carlosarauj@users.noreply.github.com>
2024-03-20 19:05:32 -0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,15 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade")
+
+print(nome)
+print(idade)
+
+print(f"Seu nome é: {nome} e sua idade é: {idade}")
+
+#usando comentário
+
+n1= int(input("Digite um número"))
+n2= int(input("Digite outro número"))
+
+res = n1 + n2
+print(res)
